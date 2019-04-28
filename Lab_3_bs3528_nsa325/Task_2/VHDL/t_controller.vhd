61d54c1fae4e31d1f4d932fc6c5a9816411ca76f
Barkin Simsek <barkin.simsek@hotmail.com>
2019-04-28 10:54:54 +0400

    added t_controller

---
+++ Lab_3_bs3528_nsa325/Task_2/VHDL/t_controller.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:54:38 04/28/2019 
+-- Design Name: 
+-- Module Name:    t_controller - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity t_controller is
+end t_controller;
+
+architecture Behavioral of t_controller is
+
+begin
+
+
+end Behavioral;
+
