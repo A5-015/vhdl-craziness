c54921a38a93e78b0fd0962e422b0d5eeaa621bf
Barkin Simsek <barkin.simsek@hotmail.com>
2019-05-04 03:11:38 +0400

    Added project 4 files

---
+++ Lab_4_bs3528_nsa325/VHDL/alu_8_bit.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    03:10:21 05/04/2019 
+-- Design Name: 
+-- Module Name:    alu_8_bit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity alu_8_bit is
+end alu_8_bit;
+
+architecture Behavioral of alu_8_bit is
+
+begin
+
+
+end Behavioral;
+
