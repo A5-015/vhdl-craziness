409a7fd1d276ecbf62b4610fbd4f101249222be5
Barkin Simsek <barkin.simsek@hotmail.com>
2019-05-06 15:16:15 +0400

    fixed display

--- Lab_4_bs3528_nsa325/VHDL/top_FPGA.vhd
+++ Lab_4_bs3528_nsa325/VHDL/top_FPGA.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    00:57:27 05/06/2019 
+-- Create Date:    14:57:18 05/06/2019 
 -- Design Name: 
--- Module Name:    display_control_unit - Behavioral 
+-- Module Name:    top_FPGA - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -15,8 +15,8 @@
 -- Revision: 
 -- Revision 0.01 - File Created
 -- Additional Comments: 
+--
 ----------------------------------------------------------------------------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.common.all;
@@ -30,19 +30,27 @@ use work.common.all;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity display_control_unit is
-    Port ( clk : in STD_LOGIC;
-			  PI_in1 : in  STD_LOGIC_VECTOR (3 downto 0); 
-           PI_in2 : in  STD_LOGIC_VECTOR (3 downto 0);
-           PI_sel : in  STD_LOGIC_VECTOR (2 downto 0);
-           PO_seg : out  STD_LOGIC_VECTOR (0 to 7);     -- turning on/off individual leds on selected 7 segment display
-           PO_an : out  STD_LOGIC_VECTOR (3 downto 0)); -- selecting one of the 7 segment displays
-end display_control_unit;
+entity top_FPGA is
+end top_FPGA;
+
+architecture Behavioral of top_FPGA is
 
 
-architecture Behavioral of display_control_unit is
+component display_control_unit is
+    Port ( clk : in STD_LOGIC); -- selecting one of the 7 segment displays
+end component;
+
+
 
 begin
 
 
+-------------------------
+-- component instances --
+-------------------------
+display_control_unit_inst : display_control_unit
+	port map (PC_current, instruction);
+
+
 end Behavioral;
+
